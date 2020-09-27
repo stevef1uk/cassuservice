6fe6014a430159324c171dbd10ae37af6e5373a3
stevef1uk <stevef1uk@googlemail.com>
2020-09-27 13:39:19 +0000

    Added test script for GitPod with Astra

---
+++ env.sh
@@ -0,0 +1,8 @@
+# These values need to be set to those of your Astra Database. 
+export CASSANDRA_SERVICE_HOST=
+export PORT=5000
+export HOST=0.0.0.0
+export CASSANDRA_USERNAME=
+export ASTRA_PORT=
+export CASSANDRA_PASSWORD=''
+export ASTRA_SECURE_CONNECT_PATH=
